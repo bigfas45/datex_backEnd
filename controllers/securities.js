44766140e723e06b2af276a48ca87e79e5272c64
Dimeji <afasina@nasdng.com>
2020-03-02 08:07:41 +0100

    Updated version of security modules

--- controllers/securities.js
+++ controllers/securities.js
@@ -165,22 +165,61 @@ exports.performanceEndByDateSecurity = (req, res, next, date ) => {
     
 let sql = "SELECT general_market_summary.Date as Date1, general_market_summary.Security, general_market_summary.`Security Name` as security_name, general_market_summary.`Open Price` as openprice , general_market_summary.`Close Price` as closeprice, general_market_summary_2.Date, general_market_summary_2.Security, general_market_summary_2.`Open Price` as openprice2 , general_market_summary_2.`Close Price` as closeprice2, market_activity_sheet.SYMBOL as symbol2 , SUM(market_activity_sheet.VOLUME) as volume FROM `general_market_summary` LEFT JOIN general_market_summary_2 ON general_market_summary.Security = general_market_summary_2.Security RIGHT JOIN market_activity_sheet ON market_activity_sheet.SYMBOL = general_market_summary.Security WHERE general_market_summary.Date=? AND general_market_summary_2.Date =? AND market_activity_sheet.DATE  BETWEEN ? AND ? GROUP By general_market_summary.Security ORDER BY general_market_summary.Security"
     let query = db.query(sql, [date1 , date2, date1, date2], (err, results) => {
-      
         if (err || !results) {
             return res.status(400).json({
                 error: 'not found'
             });
         }
-
-            
         req.security = results;
+    next();
+        
+    });
+};
+exports.performanceEndSecurity = (req, res) => {
+    return res.json(req.security);
+    next();
+};
+
+
+exports.totalBidsById = (req, res, next, id ) => {
+let sql = "SELECT `symbol`, `bidprice`, `volume` FROM `bids` WHERE symbol=? ORDER BY bidprice ASC"
+    let query = db2.query(sql, [id], (err, results) => {
+        if (err || !results) {
+            return res.status(400).json({
+                error: 'not found'
+            });
+        }
+        req.bids = results;
       
     next();
         
     });
 };
 
-exports.performanceEndSecurity = (req, res) => {
-    return res.json(req.security);
+exports.totalBids = (req, res) => {
+    return res.json(req.bids);
     next();
 };
+
+
+exports.totalOffersById = (req, res, next, id ) => {
+    
+    
+    
+    let sql = "SELECT `symbol`,`askprice`,`volume` FROM `offers` WHERE `symbol`=? ORDER BY askprice DESC"
+        let query = db2.query(sql, [id], (err, results) => {
+            if (err || !results) {
+                return res.status(400).json({
+                    error: 'not found'
+                });
+            }
+            req.offers = results;
+        next();
+            
+        });
+    };
+    
+    exports.totalOffers = (req, res) => {
+        return res.json(req.offers);
+        next();
+    };
--- routes/securities.js
+++ routes/securities.js
@@ -6,7 +6,7 @@ const {
     symbols, securityData, secSymById, secSymByIdMcap, securityMcap, secSymByIdTtrades, 
     totalTrade, secDealsById, totalDeals, performanceStartByDate, 
     performanceStart, performanceEndByDate, performanceEnd, performanceEndByDateSecurity,
-     performanceStartByDateSecurity, performanceEndSecurity, performanceStartSecurity} = require('../controllers/securities');
+     performanceStartByDateSecurity, performanceEndSecurity, performanceStartSecurity, totalBidsById, totalBids,totalOffers, totalOffersById} = require('../controllers/securities');
 
 router.get('/security/symbol',  symbols);
 router.get('/security/:secId',  securityData);
@@ -14,6 +14,9 @@ router.get('/security/chart/:secId',  securityData);
 router.get('/security/mcap/:secIdMcap',  securityMcap);
 router.get('/security/totalTrade/:secIdTtrades',  totalTrade);
 router.get('/security/totalDeals/:secDealsId',  totalDeals);
+router.get('/security/totalBids/:totalBidsId',  totalBids);
+router.get('/security/totalOffers/:totalOffersId',  totalOffers);
+
 router.get('/security/performanceStart/:performanceStartDate',  performanceStart);
 
 router.get('/security/performanceEnd/:performanceEndDate',  performanceEnd);
@@ -30,6 +33,8 @@ router.param('secId', secSymById);
 router.param('secIdMcap', secSymByIdMcap);
 router.param('secIdTtrades', secSymByIdTtrades);
 router.param('secDealsId', secDealsById);
+router.param('totalBidsId', totalBidsById);
+router.param('totalOffersId', totalOffersById);
 router.param('performanceStartDate', performanceStartByDate);
 router.param('performanceEndDate', performanceEndByDate);
 router.param('performanceEndDateSecurity', performanceEndByDateSecurity);
